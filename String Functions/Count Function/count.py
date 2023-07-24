c335bd8f1271c9c21b5a30dce4801cf2a441b371
TheGeekManu <131792695+TheGeekManu@users.noreply.github.com>
2023-07-24 15:03:19 +0300

    Create count.py

---
+++ String Functions/Count Function/count.py
@@ -0,0 +1,6 @@
+#Create a test string for use with the count function
+testString = "I own two apartments and six cars"
+
+#Count the number of occurences the letter 's' appears in testString
+counter = testString.count("s")
+print(counter)

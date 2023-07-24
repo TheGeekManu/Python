d2f7fca55f50671af234bf94b09d99dabdb5610d
TheGeekManu <131792695+TheGeekManu@users.noreply.github.com>
2023-07-24 14:52:51 +0300

    Create in.py

---
+++ String Functions/In Function/in.py
@@ -0,0 +1,8 @@
+#A string to check against.
+testString = "I am learning a post graduate course"
+
+#Return message if True or False
+if('graduate' in testString):
+    print("True")
+else:
+    print("False")

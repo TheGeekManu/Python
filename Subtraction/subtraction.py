2458704177eab8c6db71f42545f22a9e56bb57dc
TheGeekManu <131792695+TheGeekManu@users.noreply.github.com>
2023-07-24 14:23:08 +0300

    Create subtraction.py

---
+++ Subtraction/subtraction.py
@@ -0,0 +1,15 @@
+import math
+
+digitOne = input('Enter the first number: ')
+digitTwo = input('Enter the second number: ')
+
+try:
+    floatInputOne = float(digitOne)
+    floatInputTwo = float(digitTwo)
+    print("You entered " + digitOne + " and " + digitTwo)
+    result = floatInputOne - floatInputTwo
+    print()
+    print("The result of " + str(floatInputOne) +" - " + str(floatInputTwo) + " is: " + str(result))
+
+except ValueError:
+    print("Float Please")

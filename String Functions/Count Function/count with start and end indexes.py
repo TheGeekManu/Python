07b459855842681679692ca2311d7a55ffcb1ed1
TheGeekManu <131792695+TheGeekManu@users.noreply.github.com>
2023-07-24 15:12:15 +0300

    Create count with start and end indexes.py

---
+++ String Functions/Count Function/count with start and end indexes.py
@@ -0,0 +1,6 @@
+#Create a test string for use with the count function
+testString = "I own two apartments and six cars"
+
+#Count the number of occurences of 'a' in our string given a start and end indexes
+counter = testString.count("a",9,19)
+print(counter)

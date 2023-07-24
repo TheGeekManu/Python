556ec93e54daca1527cacf4e0af0d03cc45d7192
TheGeekManu <131792695+TheGeekManu@users.noreply.github.com>
2023-07-24 17:48:35 +0300

    Create join.py

---
+++ String Functions/Join Function/join.py
@@ -0,0 +1,15 @@
+#Create a random tuple
+mountainTuple = ("Everest", "Kilimanjaro", "Alps")
+
+#Join the tuple using '_' as the separator
+x = "_".join(mountainTuple)
+print(x)
+print('\n')
+
+#Create a random dictionary
+name_country_Dictionary = {"name": "Alex", "country": "Spain"}
+
+#Join the tuple using '/' as the separator
+separator = "/"
+x = separator.join(name_country_Dictionary)
+print(x)

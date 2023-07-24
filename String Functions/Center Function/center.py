1d7796e87635fef44cf18f2619bb356d4d2bbd56
TheGeekManu <131792695+TheGeekManu@users.noreply.github.com>
2023-07-24 15:38:07 +0300

    Create center.py

---
+++ String Functions/Center Function/center.py
@@ -0,0 +1,11 @@
+#Create a random string
+randomText = "shark"
+
+#Print the randomText centered
+centered = randomText.center(20)
+print(centered)
+print("\n")
+
+#Print the randomText centered with a padding of '-'
+centered = randomText.center(20, "-")
+print(centered)

8bc24d77f0b8ae3ff87320e07858679adcc98a40
TheGeekManu <131792695+TheGeekManu@users.noreply.github.com>
2023-07-24 14:20:02 +0300

    Update addition.py

--- Addition/addition.py
+++ Addition/addition.py
@@ -4,12 +4,12 @@ digitOne = input('Enter the first number: ')
 digitTwo = input('Enter the second number: ')
 
 try:
-    integerInputOne = int(digitOne)
-    integerInputTwo = int(digitTwo)
+    floatInputOne = float(digitOne)
+    floatInputTwo = float(digitTwo)
     print("You entered " + digitOne + " and " + digitTwo)
-    result = integerInputOne + integerInputTwo
+    result = floatInputOne + floatInputTwo
     print()
-    print("The result of " + str(integerInputOne) +" + " + str(integerInputTwo) + " is: " + str(result))
+    print("The result of " + str(floatInputOne) +" + " + str(floatInputTwo) + " is: " + str(result))
 
 except ValueError:
     print("Integers Please")

2a491fc2df157519c287400b71094758538638f6
TheGeekManu <131792695+TheGeekManu@users.noreply.github.com>
2023-07-24 15:29:04 +0300

    Create casefold.py

---
+++ String Functions/Casefold/casefold.py
@@ -0,0 +1,5 @@
+#Create a literal string
+randomText = "Happy BIRTHDAY To You And ENJOY anOTHEr great YeAr!"
+
+#Use the casefold function to convert all letters to lowercase
+print(randomText.casefold())

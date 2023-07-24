86c4491e4bc7527bb1404520f0c57e3b446f47e3
TheGeekManu <131792695+TheGeekManu@users.noreply.github.com>
2023-07-24 15:51:57 +0300

    Update encode.py

--- String Functions/Encode Function/encode.py
+++ String Functions/Encode Function/encode.py
@@ -1,1 +1,19 @@
+#Create a literal string
+testString = "My friend is called Stållion Krughåz"
+
+#Use the encode function without parameters.
+#Note the default choice is UTF-8 when no encoding parameter is set.
+print(testString.encode())
+print("\n")
+
+#Print encoded values under ASCII encoding with error parameters set
+print(testString.encode(encoding="ascii",errors="backslashreplace"))
+print("\n")
+print(testString.encode(encoding="ascii",errors="ignore"))
+print("\n")
+print(testString.encode(encoding="ascii",errors="namereplace"))
+print("\n")
+print(testString.encode(encoding="ascii",errors="replace"))
+print("\n")
+print(testString.encode(encoding="ascii",errors="xmlcharrefreplace"))
 

354f6af62cb9c5238c0b28067cd95037710153b1
TheGeekManu <131792695+TheGeekManu@users.noreply.github.com>
2023-07-24 14:17:57 +0300

    Update division.py

--- Division/division.py
+++ Division/division.py
@@ -3,14 +3,14 @@ import math
 def division():
     """ This code provides float division arithmetic functionality """
 
-    integerInputOne = float(input('Enter the first number: '))
-    integerInputTwo = float(input('Enter the second number: '))
-    print("You entered " + str(integerInputOne) + " and " + str(integerInputTwo))
+    floatInputOne = float(input('Enter the first number: '))
+    floatInputTwo = float(input('Enter the second number: '))
+    print("You entered " + str(floatInputOne) + " and " + str(floatInputTwo))
 
     try:
-        result = integerInputOne / integerInputTwo
+        result = floatInputOne / floatInputTwo
         print()
-        print("The results of the division of " + str(integerInputOne)+ " and " +str(integerInputTwo) + " is: " + str(result))
+        print("The results of the division of " + str(floatInputOne)+ " and " +str(floatInputTwo) + " is: " + str(result))
 
     except ValueError:
         print("Integers Please")

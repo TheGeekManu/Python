5312e3c797656c82c490056afe8a2b31ca9cd091
TheGeekManu <131792695+TheGeekManu@users.noreply.github.com>
2023-08-02 14:22:16 +0200

    Update array_with_for_loop.py

--- Arrays/Single Dimension/array_with_for_loop.py
+++ Arrays/Single Dimension/array_with_for_loop.py
@@ -1,1 +1,28 @@
+#Create Array.
+Houses = ["Mansion","Bungalow","Apartment"]
 
+#Append Function
+Houses.append("Mud hut")
+
+#Use a for loop to print out the array items.
+for x in Houses:
+    print(x)
+
+#Calculate Array Length.
+print(len(Houses))
+
+#Remove array elements.
+Houses.pop(0)
+
+#Second Method
+Houses.remove("Apartment")
+
+#Extend the array
+Houses.extend("Null")
+
+#Use a for loop to print out the array items.
+for x in Houses:
+    print(x)
+
+#Calculate Array Length.
+print(len(Houses))
